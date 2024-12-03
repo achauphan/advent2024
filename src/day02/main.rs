c9d1991c97635e5e3789663adac4be12099509ca
Anderson <chauphana@outlook.com>
2024-12-02 20:19:52 -0600

    Day 2, problem 1 completed

--- src/day02/main.rs
+++ src/day02/main.rs
@@ -3,77 +3,83 @@ use std::fs::read_to_string;
 // - Levels are either all increasing or all decreasing
 // - Any two adjacent levels differ by at least 1 and at most 3
 
-
 fn read_lines(filename: &str) -> Vec<String> {
-	let mut lines = Vec::new();
-	for line in read_to_string(filename).unwrap().lines() {
-		lines.push(line.to_string());
-	}
-	return lines;
+    let mut lines = Vec::new();
+    for line in read_to_string(filename).unwrap().lines() {
+        lines.push(line.to_string());
+    }
+    return lines;
 }
 
 
-fn check_all_increase_or_decrease(report: Vec<i32>) -> bool {
-	let mut increasing: bool = true;
-	let mut decreasing: bool = true;
+fn check_report_increasing_or_decreasing(report: &Vec<i32>) -> bool {
+    let mut is_increasing: bool = true;
+    let mut is_decreasing: bool = true;
 
-	for level_window in report.windows(2) {
-		if level_window[0] < level_window[1] {
-			decreasing = false;		
-		} else if level_window[0] > level_window[1] {
-			increasing = false;
-		} else {
-			continue;
-		}
+    for level_window in report.windows(2) {
+        if level_window[0] < level_window[1] {
+            is_decreasing = false;     
+        } else if level_window[0] > level_window[1] {
+            is_increasing = false;
+        } else {
+            continue;
+        }
 
-		// inverse of FALSE OR case only
-		if !(increasing || decreasing) { break; }
-	}
+        // inverse of FALSE OR case only
+        if !(is_increasing || is_decreasing) { break; }
+    }
 
-//	println!("Report: {:?} increasing: {}", report, increasing);
-//	println!("Report: {:?} decreasing: {}", report, decreasing);
-//	println!("FINAL REPORT: {}", increasing != decreasing);
-	return increasing != decreasing;
+//  println!("Report: {:?} increasing: {}", report, increasing);
+//  println!("Report: {:?} decreasing: {}", report, decreasing);
+//  println!("FINAL REPORT: {}", increasing != decreasing);
+    return is_increasing != is_decreasing;
 }
 
-fn check_margin() {
-
+fn check_report_margins(report: &Vec<i32>, min: i32, max: i32) -> bool {
+    for level_window in report.windows(2) {
+        let diff: i32 = (level_window[0] - level_window[1]).abs();
+        if  !(diff >= min && diff <= max) {
+            return false;
+        }
+    }
+    return true;
 }
 
 // https://stackoverflow.com/questions/34090639/how-do-i-convert-a-vector-of-strings-to-a-vector-of-integers-in-a-functional-way
-fn problem_1(reports: Vec<String>) -> i32 {
-	let mut num_safe: i32 = 0;
+fn problem_1(reports: &Vec<String>) -> i32 {
+    let mut num_safe: i32 = 0;
 
-	for report in reports.iter() {
-		// Convert delimited string to i32 vector
-		let report_parsed: Vec<i32> = report.split_whitespace()
-											.filter_map(|x| x.parse().ok())
-											.collect();		
-		//println!("report_parsed: {:?}", report_parsed);
-		if !check_all_increase_or_decrease(report_parsed) {
-			continue;
-		}
+    for report in reports.iter() {
+        // Convert delimited string to i32 vector
+        let report_parsed: Vec<i32> = report.split_whitespace()
+                                            .filter_map(|x| x.parse().ok())
+                                            .collect();     
+        //println!("report_parsed: {:?}", report_parsed);
+        if !check_report_increasing_or_decreasing(&report_parsed) {
+            continue;
+        }
+        
+        let min_margin: i32 = 1;
+        let max_margin: i32 = 3;
 
-		if !check_margin() {
-			continue;
-		}
-
-		
-	}
-
-	return num_safe;
+        if !check_report_margins(&report_parsed, min_margin, max_margin) {
+            continue;
+        }
+        num_safe += 1;
+    }
+    return num_safe;
 }
 
-fn problem_2() {
-
+fn problem_2(reports: &Vec<String>) -> i32 {
+    return 1;
 }
 
 
 fn main() {
-	let input_filename = String::from("input.txt");
-	let input_lines = read_lines(&input_filename);
+    let input_filename = String::from("input.txt");
+    let input_lines = read_lines(&input_filename);
 
-	problem_1(input_lines);
-
+    println!("Problem 1: {}, Problem 2: {}",
+        problem_1(&input_lines), problem_2(&input_lines));
 
 }
